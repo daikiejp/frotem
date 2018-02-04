4655b8671df4476b18dd0cd391401ccd004c5e9b
Danny Davila <danny@ecuachina.com>
2018-02-04 23:23:40 +0800

    [Feature] Added Font-Awesome 5

--- src/index.js
+++ src/index.js
@@ -4,3 +4,6 @@ window.$ = $;
 
 require('bootstrap/dist/css/bootstrap.min.css');
 require('./css/main.css');
+
+import {faGlobe} from '@fortawesome/fontawesome-free-solid';
+import fontawesome from '@fortawesome/fontawesome';
